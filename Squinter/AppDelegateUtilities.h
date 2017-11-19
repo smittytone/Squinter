5d0b59855243511c29b95c7be45a0ae7545a015b
Tony Smith <tony@electricimp.com>
2017-11-19 21:43:24 +0000

    Build 11: Spin out utility methods into separate, category file

---
+++ Squinter/AppDelegateUtilities.h
@@ -0,0 +1,40 @@
+
+
+//  Created by Tony Smith on 09/02/2015.
+//  Copyright (c) 2015-17 Tony Smith. All rights reserved.
+
+
+#import "AppDelegate.h"
+
+
+@interface AppDelegate(AppDelegateUtilities)
+
+
+// File Path Methods
+
+- (NSString *)getRelativeFilePath:(NSString *)basePath :(NSString *)filePath;
+- (NSString *)getPathDelta:(NSString *)basePath :(NSString *)filePath;
+- (NSInteger)numberOfFoldersInPath:(NSString *)path;
+- (NSString *)getAbsolutePath:(NSString *)basePath :(NSString *)relativePath;
+- (NSString *)getPrintPath:(NSString *)projectPath :(NSString *)filePath;
+- (NSData *)bookmarkForURL:(NSURL *)url;
+- (NSURL *)urlForBookmark:(NSData *)bookmark;
+
+// Logging Utility Methods
+
+- (void)parseLog;
+
+// Utility Methods
+
+- (id)getValueFrom:(NSDictionary *)apiDict withKey:(NSString *)key;
+- (NSString *)convertDevicegroupType:(NSString *)type :(BOOL)back;
+- (Project *)getParentProject:(Devicegroup *)devicegroup;
+- (NSDate *)convertTimestring:(NSString *)dateString;
+- (NSString *)getErrorMessage:(NSUInteger)index;
+- (NSArray *)displayDescription:(NSString *)description :(NSInteger)maxWidth :(NSString *)spaces;
+- (void)setDevicegroupDevices:(Devicegroup *)devicegroup;
+- (void)setWorkingDirectory:(NSArray *)urls;
+- (NSString *)getFontName:(NSInteger)index;
+- (NSInteger)perceivedBrightness:(NSColor *)colour;
+
+@end
