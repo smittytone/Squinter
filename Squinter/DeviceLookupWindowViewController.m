b0caab3bc32f099b2107cc756f0fde8fc2af76c3
smittytone <smittytone@icloud.com>
2019-04-17 10:21:52 +0100

    Add device searches by agent ID

--- Squinter/DeviceLookupWindowViewController.m
+++ Squinter/DeviceLookupWindowViewController.m
@@ -50,6 +50,10 @@
         // the latter case, and we use this to set the auxilliary info column
         // head title etc.
         
+        // FROM 2.3.128
+        // Match on agent ID as well as MAC and Device ID, and make sure device ID and
+        // MAC searches are NOT case-sensitive        
+        
         // Stop the text field from working during processing?
         
         [entryField refusesFirstResponder];
@@ -79,25 +83,58 @@
             // NOTE Check for NULL entries
             
             NSString *did = [device objectForKey:@"id"];
+            NSString *sdid = [device objectForKey:@"id"];
+            did = [did substringToIndex:(keyed.length <= did.length ? keyed.length : did.length)];
+            
             NSString *mac = [device valueForKeyPath:@"attributes.mac_address"];
-            if ((NSNull *)mac == [NSNull null]) mac = @"Unknown";
-            mac = [mac stringByReplacingOccurrencesOfString:@":" withString:@""];
-
+            NSString *smac = @"";
+            if ((NSNull *)mac == [NSNull null])
+            {
+                mac = nil;
+            }
+            else
+            {
+                smac = [mac copy];
+                mac = [mac stringByReplacingOccurrencesOfString:@":" withString:@""];
+                [mac substringToIndex:(keyed.length <= mac.length ? keyed.length : mac.length)];
+            }
+            
+            NSString *aid = [device valueForKeyPath:@"attributes.agent_id"];
+            NSString *said = @"No Agent";
+            if ((NSNull *)aid == [NSNull null])
+            {
+                aid = nil;
+            }
+            else
+            {
+                said = [aid copy];
+                aid = [aid substringToIndex:(keyed.length <= aid.length ? keyed.length : aid.length)];
+            }
+            
+            /*
             NSString *sdid = searchOnDeviceId
                 ? [did substringToIndex:(keyed.length <= did.length ? keyed.length : did.length)]
             : [mac substringToIndex:(keyed.length <= mac.length ? keyed.length : mac.length)];
-
-            if ([sdid compare:keyed] == NSOrderedSame)
+            */
+            
+            bool matched = NO;
+            
+            if ([keyed.lowercaseString compare:did] == NSOrderedSame) matched = YES;
+            if (mac != nil && [keyed.lowercaseString compare:mac] == NSOrderedSame) matched = YES;
+            if (aid != nil && [keyed compare:aid] == NSOrderedSame) matched = YES;
+            
+            if (matched)
             {
                 // Devices that match are added to 'listedDevices' which
                 // is then read by the table as a data source
                 
                 NSString *name = [device valueForKeyPath:@"attributes.name"];
-                if ((NSNull *)name == [NSNull null]) name = did;
+                if ((NSNull *)name == [NSNull null]) name = sdid;
 
                 NSDictionary *d = @{ @"name" : name,
-                                     @"id"   : did,
-                                     @"mac"  : mac };
+                                     @"id"   : sdid,
+                                     @"mac"  : smac,
+                                     @"aid"  : said };
                 [listedDevices addObject:d];
             }
         }
@@ -171,7 +208,7 @@
             return cell;
         }
     }
-    else
+    else if ([tableColumn.identifier compare:@"devicelookup_column_mac"] == NSOrderedSame)
     {
         // Display the device's MAC in the MAC column
         
@@ -181,6 +218,20 @@
         {
             NSDictionary *device = [listedDevices objectAtIndex:row];
             cell.textField.stringValue = [device objectForKey:@"mac"];
+            cell.textField.enabled = NO;
+            return cell;
+        }
+    }
+    else
+    {
+        // Display the device's Agent ID in the aid column
+        
+        NSTableCellView *cell = [tableView makeViewWithIdentifier:@"devicelookup_cell_aid" owner:nil];
+        
+        if (cell != nil)
+        {
+            NSDictionary *device = [listedDevices objectAtIndex:row];
+            cell.textField.stringValue = [device objectForKey:@"aid"];
             cell.textField.enabled = NO;
             return cell;
         }
